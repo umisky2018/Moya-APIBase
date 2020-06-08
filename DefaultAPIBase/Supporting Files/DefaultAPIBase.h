76545f80fe54fc567b1f4b41915df139653e2084
umisky <umiskyhorizon@gmail.com>
2020-06-08 10:59:20 +0800

    预备拆分

---
+++ DefaultAPIBase/Supporting Files/DefaultAPIBase.h
@@ -0,0 +1,19 @@
+//
+//  DefaultAPIBase.h
+//  DefaultAPIBase
+//
+//  Created by umisky on 2020/6/8.
+//  Copyright © 2020 umisky. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DefaultAPIBase.
+FOUNDATION_EXPORT double DefaultAPIBaseVersionNumber;
+
+//! Project version string for DefaultAPIBase.
+FOUNDATION_EXPORT const unsigned char DefaultAPIBaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DefaultAPIBase/PublicHeader.h>
+
+
